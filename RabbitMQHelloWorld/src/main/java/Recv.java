23639ae3954246250832399ba688463c33eba426
thomassi <669285@stud.hvl.no>
2022-10-19 15:03:39 +0200

    Finish Work Queues tutorial

--- RabbitMQHelloWorld/src/main/java/Recv.java
+++ RabbitMQHelloWorld/src/main/java/Recv.java
@@ -2,13 +2,13 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DeliverCallback;
-import org.apache.log4j.BasicConfigurator;
+//import org.apache.log4j.BasicConfigurator;
 
 public class Recv {
     private final static String QUEUE_NAME = "hello";
 
     public static void main(String[] argv) throws Exception {
-        BasicConfigurator.configure();
+  //      BasicConfigurator.configure();
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         Connection connection = factory.newConnection();
